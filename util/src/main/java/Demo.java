e21421ff61a8bf03f9c01e061f88cfbecd745fa5
longchen <1138312802@qq.com>
2021-08-20 18:02:52 +0800

    1

---
+++ util/src/main/java/Demo.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author Long Chen
+ * @description
+ * @date 2021-08-20 16:05
+ */
+
+public class Demo {
+
+	public static void main(String[] args) {
+		String str = "`aaa``bbb``ccc`";
+		str = str.substring(1, str.length() - 1);
+		Arrays.asList(str.split("``")).forEach(System.out::println);
+	}
+}
